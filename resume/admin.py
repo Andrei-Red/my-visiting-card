2a91b2e33cf435e7ed9ed2cc1abc7824e3282dbc
Andrei <denisenko.andrei.da@gmail.com>
2020-04-02 16:00:25 +0300

    Revert "My viziting card"
    This reverts commit 5dd62bf900104e6b361c92400f23a3dc93d4e05d.

--- resume/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import PlaceOfWork, Detail, MyContact
-
-
-admin.site.register(PlaceOfWork)
-admin.site.register(Detail)
-admin.site.register(MyContact)
--- resume/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', welcome, name='welcome'),
-    path('work/', work, name='work'),
-    path('work/<int:work_id>/', detail_my_work, name='detail_my_work'),
-    path('projects/', project, name='project'),
-]
