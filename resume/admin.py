8e57927cddf80e5bd2f0344ed39ed3373a954ee0
Andrei <denisenko.andrei.da@gmail.com>
2020-04-02 16:01:35 +0300

    Revert "Revert "My viziting card""
    This reverts commit 2a91b2e33cf435e7ed9ed2cc1abc7824e3282dbc.

---
+++ resume/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import PlaceOfWork, Detail, MyContact
+
+
+admin.site.register(PlaceOfWork)
+admin.site.register(Detail)
+admin.site.register(MyContact)
---
+++ resume/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class PlaceOfWork(models.Model):
+    organization = models.CharField('organization', max_length=100)
+
+    def __str__(self):
+        return self.organization
+
+
+class Detail(models.Model):
+    organization = models.ForeignKey(PlaceOfWork, on_delete=models.CASCADE)
+    position = models.CharField('position', max_length=100)
+    responsibilities = models.TextField('responsibilities')
+    started_work = models.DateTimeField('started_work')
+    finished_work = models.DateTimeField('finished_work')
+
+    def __str__(self):
+        return self.position
+
+
+class MyContact(models.Model):
+    email = models.CharField('email', max_length=100)
+    phone = models.CharField('phone', max_length=100)
